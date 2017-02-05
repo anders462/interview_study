460fe8f912b1af4c05e92a8b686ee537309862ea
anders462 <anders462@gmail.com>
2017-02-05 15:49:30 -0800

    added methods to linked list

--- data_structures/sinle-linked-list.js
+++ data_structures/sinle-linked-list.js
@@ -1,4 +1,9 @@
+
+
+
 class Node {
+  //node:
+  //data: stores value, next: points to next object
   constructor(data){
     this.data = data;
     this.next = null;
@@ -6,19 +11,21 @@ class Node {
 }
 
 class SinglyList {
+  //_length: number of nodes in LinkedList
+  //head: the first node in the list
   constructor() {
-    this.length = 0;
+    this._length = 0;
     this.head = null;
   }
 
+  //add: add a new node with data = value to end of list
   add(value) {
     const node = new Node(value);
     let currentNode = this.head;
     //list is empty
     if (!currentNode) {
       this.head = node;
-      ++this.length
-
+      ++this._length
       return node;
     }
     //list is not empty
@@ -27,13 +34,13 @@ class SinglyList {
     }
 
     currentNode.next = node
-    ++this.length;
+    ++this._length;
     return node;
   };
 
   searchNodeAt(position) {
     let currentNode = this.head;
-    const length = this.length;
+    const length = this._length;
     let count = 1;
     const message = {failure: "Failure, non existant node"};
 
@@ -51,7 +58,39 @@ class SinglyList {
 
   remove(position) {
     let currentNode = this.head;
-    let length = this.length;
+    let prevoiusNode = null;
+    let deletedNode = null;
+    let length = this._length;
+    let count = 1;
+
+    const message = {failure: "Failure, non existant node"}
+
+    if (length === 0 || position < 0 || position > length) {
+      throw new Error(message.failure);
+    }
+
+    if (position === 1) {
+      --this._length;
+      this.head = currentNode.next;
+      deletedNode = currentNode;
+      currentNode = null;
+      deletedNode.next =  null;
+      return deletedNode;
+    }
+
+    while (currentNode.next && count < position) {
+     prevoiusNode = currentNode;
+     currentNode = currentNode.next
+     count++
+    }
+
+    prevoiusNode.next = currentNode.next;
+    deletedNode = currentNode;
+    currentNode = null;
+    deletedNode.next =  null;
+    --this._length;
+    return deletedNode;
+
   }
 }
 
@@ -60,4 +99,5 @@ const list = new SinglyList();
 list.add(12);
 list.add(13);
 list.add(14)
-console.log(list.searchNodeAt(2))
+list.add(15);
+console.log(list.remove(1), "rest of list", list)
